de6f7b082b807107802fbebd7851fe92f9017425
yashsakalley-1997 <sakalley.yash@gmail.com>
2022-01-21 00:09:48 +0530

    Added dropdown functionality

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ function appendData(data,location){
 }
 
 
-function appendProducts(data,location){
+function appendProducts(data,location,dropdown){
     let store_names = Object.keys(data);
     for(let i = 0;i<store_names.length;i++){
         // First Heading div.
@@ -57,11 +57,61 @@ function appendProducts(data,location){
             p2.textContent = elem['name'];
             div2.append(img,p2)
             div1.append(div2)
+            div2.addEventListener("click",()=>{
+                localStorage.setItem("display_obj",JSON.stringify(elem))
+                let modal = document.getElementById("myModal");
+                let modal_div = document.querySelector(".display_content");
+                appendModal(elem,modal_div,dropdown)
+                appendModalData(data[store_names[i]])
+                modal.style.display = "block";
+            })
         })
         location.append(heading_div,div1,hr)
     }
 }
 
 
+function appendModal(data,location,dropdown){
+    location.innerHTML = "";
+    let div = document.createElement("div");
+    let side_div = document.createElement("div");
+    let button_div = document.createElement("div");
+    dropdown.style.display = "block";
+    let p1 = document.createElement("p")
 
+    p1.textContent = data['name'];
+
+    let p2 = document.createElement("p")
+    p2.textContent = "$"+Math.floor(Math.random() * 5) + 1;
+
+    let btn = document.createElement("button")
+    btn.id = "cart_btn";
+    btn.textContent = "Add to Cart"
+
+    button_div.append(dropdown,btn)
+    let img = document.createElement("img");
+    img.src = data['img'];
+
+    side_div.append(p1,p2,button_div)
+    div.append(img,side_div)
+    div.style.display = "flex"
+    div.style.justifyContent = "space-around"
+    location.append(div);
+}
+
+function appendModalData(data){
+    let main_div = document.querySelector(".recommended")
+    main_div.innerHTML = "";
+    data.forEach(({img,name}) => {
+       let div = document.createElement("div");
+       let img1 = document.createElement("img");
+       img1.src = img;
+       let p1 = document.createElement("p");
+       p1.textContent = name;
+       let btn = document.createElement("button");
+       btn.textContent = "Add to Cart";
+       div.append(img1,p1,btn);
+       main_div.append(div)
+    });
+}
 export {getData,appendData,appendProducts};
