0d66ccfc3a0f0720bb06009408a35a8950322c61
Maddy <95930594+Maddy-O@users.noreply.github.com>
2022-01-23 11:23:47 +0530

    final
    final

--- components/header.js
+++ components/header.js
@@ -8,18 +8,18 @@ function header(term) {
                     <img width="24" height="24" viewBox="0 0 24 24" src="https://img.icons8.com/material-rounded/24/000000/menu--v1.png" />
                 </div>
                 <div class="logo">
-                    <a href="/HomePage/index.html">
+                    <a href="index.html">
                         <img src="https://www.instacart.com/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg" alt="logo">
                     </a>
                 </div>
             </div>
             <div class="right">
-                <button class="login_btn">
-                    <span>Log in</span>
-                </button>
-                <button class="signup_btn">
-                    <span>Sign up</span>
-                </button>
+                    <button class="login_btn">
+                        <span>Log in</span>
+                    </button>
+                    <button class="signup_btn">
+                        <span>Sign up</span>
+                    </button>
             </div>
         </nav>
     `
